6e0c2a9c5794ff3db22019a3dc81c2f16e3ce4f2
Freeze-2 <avdigitaldesigns@gmail.com>
2021-05-22 19:33:25 +0100

    Remove method causing the application to not start

--- SS_OpenCV/MainForm.cs
+++ SS_OpenCV/MainForm.cs
@@ -287,25 +287,5 @@ namespace CG_OpenCV
             his.Show();
             Cursor = Cursors.Default;
         }
-
-        private void randomToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (img == null) // verify if the image is already opened
-            {
-                return;
-            }
-
-            Cursor = Cursors.WaitCursor; // clock cursor 
-
-            //copy Undo Image
-            imgUndo = img.Copy();
-
-            ImageClass.DetectIndependentObjects(img, img);
-
-            ImageViewer.Image = img.Bitmap;
-            ImageViewer.Refresh(); // refresh image on the screen
-
-            Cursor = Cursors.Default; // normal cursor 
-        }
     }
 }
